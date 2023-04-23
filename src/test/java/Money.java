61eb1f7fcb9510cc98e0f012b391125b2e51c210
Donghee-Jeong <weg9511@gmail.com>
2023-04-23 17:30:53 +0900

    5. 솔직히 말하자면
    테스트 코드 내의 클래스 분리 및 Franc 클래스 생성

--- src/test/java/Money.java
+++ src/test/java/Money.java
@@ -17,23 +17,11 @@ public class Money {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
-    class Dollar {
-        // 3장에서의 동시성에 대한 코드 추가에서가 아닌, 이번 Dollar 간 비교로 테스트가 바뀐 후 변경했어야 한다.
-        private int amount;
-
-        Dollar(int amount) {
-            this.amount = amount;
-        }
-
-        Dollar times(int multiplier) {
-            return new Dollar(amount * multiplier);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Dollar dollar = (Dollar) obj;
-            return amount == dollar.amount;
-        }
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
 
